bcdbdb2d9991701cb7ec0a52cab48d1f391cbead
Allison Reiss <allisonreiss1@gmail.com>
2017-11-24 21:16:59 -0800

    set up collection view

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Allison Reiss on 11/24/17.
+//  Copyright © 2017 Allison Reiss. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
